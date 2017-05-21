9341b81f0e2f4e0bc4e1a3178527c1603058b8dc
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-05-21 07:46:03 +0300

    Update vector example

--- vector-member-init/vector-member-init.cpp
+++ vector-member-init/vector-member-init.cpp
@@ -9,6 +9,15 @@ public:
     intVector = input;
   }
 
+  ~Something() {
+    for(int i = 0; i < intVector.size(); i++) {
+      if(intVector[i]){
+        cout << "Deleting " << i << "th" << " element of vector member" << endl;
+        delete intVector[i];
+      }
+    }
+  }
+
   void list() {
     for(int i = 0; i < intVector.size() ; i++) {
       cout << *intVector[i] << endl;
@@ -22,17 +31,23 @@ int main() {
   std::vector<int *> aVector;
   int *ip1 = new int;
   int *ip2 = new int;
+  int *ip3 = new int;
 
   *ip1 = 3;
   *ip2 = 7;
+  *ip3 = 5;
 
   aVector.push_back(ip1);
   aVector.push_back(ip2);
+  aVector.push_back(ip3);
 
-  Something aThing(aVector);
+  Something* aThing = new Something(aVector);
   aVector.clear();
 
-  aThing.list();
+  aThing->list();
+
+  delete aThing;
+
 
   return 0;
 }
